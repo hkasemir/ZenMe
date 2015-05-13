6a489d28d16ddf945e13dfd79d1b73dcfabe27b6
hkasemir <heidi.kasemir@gmail.com>
2015-05-13 15:30:20 -0400

    added styling and fade transitions, but there is a bug with layout of quote and author text

--- app/src/main/java/name/heidik/zenme/ZenActivity.java
+++ app/src/main/java/name/heidik/zenme/ZenActivity.java
@@ -1,25 +1,36 @@
 package name.heidik.zenme;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ZenActivity extends ActionBarActivity {
-    private TextView tvQuote;
-    private TextView tvAuthor;
+public class ZenActivity extends AppCompatActivity {
+    private TextSwitcher tvQuote;
+    private TextSwitcher tvAuthor;
     private List<Quote> quotes;
     private int quoteIndex = 0;
 
@@ -27,6 +38,9 @@ public class ZenActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
 
 
         try {
@@ -41,26 +55,67 @@ public class ZenActivity extends ActionBarActivity {
             ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             TypeReference<ArrayList<Quote>> tr = new TypeReference<ArrayList<Quote>>() {};
             quotes = mapper.readValue(json, tr);
-            Log.d("DEBUG", "quotes: " + quotes.toString());
         } catch (JsonParseException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        final Typeface ebGaramond=Typeface.createFromAsset(getAssets(),"fonts/EBGaramond-Regular.ttf");
         setContentView(R.layout.activity_zen);
-        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
-        tvQuote = (TextView) findViewById(R.id.tvQuote);
+        tvAuthor = (TextSwitcher) findViewById(R.id.tvAuthor);
+        tvQuote = (TextSwitcher) findViewById(R.id.tvQuote);
+
+        tvQuote.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView() {
+                // TODO Auto-generated method stub
+                // create new textView and set the properties like clolr, size etc
+                TextView myText = new TextView(ZenActivity.this);
+                myText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+                myText.setTypeface(ebGaramond);
+                return myText;
+            }
+        });
+
+        tvAuthor.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView() {
+                // TODO Auto-generated method stub
+                // create new textView and set the properties like clolr, size etc
+                TextView myText = new TextView(ZenActivity.this);
+                myText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+                myText.setTypeface(ebGaramond);
+                return myText;
+            }
+        });
+
+
+        Animation in = AnimationUtils.loadAnimation(this, R.anim.fade_in_custom);
+        Animation out = AnimationUtils.loadAnimation(this, R.anim.fade_out_custom);
+
+        tvQuote.setInAnimation(in);
+        tvQuote.setOutAnimation(out);
+        tvAuthor.setInAnimation(in);
+        tvAuthor.setOutAnimation(out);
+
+
+
+
 
         tvAuthor.setText(quotes.get(quoteIndex).author);
         tvQuote.setText(quotes.get(quoteIndex).quote);
 
     }
 
+
+
+
+
     public void zenAgain(View v) {
         quoteIndex = (quoteIndex + 1) % quotes.size();
-        tvAuthor.setText(quotes.get(quoteIndex).author);
         tvQuote.setText(quotes.get(quoteIndex).quote);
+        tvAuthor.setText(quotes.get(quoteIndex).author);
     }
 
 
